c336bc422048b28577bb11226e02b67a512d9843
James Twose <jamestwose@Mac-mini.home>
2023-05-18 14:22:28 +0200

    getting the freelance_nl.py script to run locally, next steps are to try to get this to run correctly on github actions

--- job_scrape/freelance_nl.py
+++ job_scrape/freelance_nl.py
@@ -1,19 +1,59 @@
-import asyncio
-from pyppeteer import launch
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
 import pandas as pd
+from datetime import datetime
+from dotenv import load_dotenv, find_dotenv
+import os
 
-async def scrape():
-    browser = await launch()
-    page = await browser.newPage()
-    await page.goto('https://mijn.freelance.nl/acquisitie/mijn-matches?s706310')
-    
-    #projects > div > div.projectlist
-    # list_content = page.querySelector("div.projectlist")
-    # print(list_content)
-    
-    print(page.content())
-    
-    await browser.close()
-    
-asyncio.get_event_loop().run_until_complete(scrape())
-    
+_ = load_dotenv(find_dotenv())
+
+with sync_playwright() as p:
+    # browser = p.chromium.launch(headless=False, slow_mo=1000)
+    browser = p.chromium.launch(headless=True, slow_mo=1000)
+    page = browser.new_page()
+    initial_url = "https://mijn.freelance.nl/"
+    page.goto(initial_url)
+    page.fill(
+        "#login > div > div > div.content > div > form > ul > li.email.required > div > div > input[type=email]",
+        os.getenv("FREELANCE_NL_USERNAME"),
+    )
+    page.fill(
+        "#login > div > div > div.content > div > form > ul > li.password.required > div > div > input[type=password]",
+        os.getenv("FREELANCE_NL_PASSWORD"),
+    )
+    page.click("#login > div > div > div.content > div > form > button")
+    page.fill(
+        "#filterform > div > div.matchfilter > div.matchfilter__query-fields.fields > div.matchfilter__query-items.items > div > div > div > div > div > div > input",
+        "python",
+    )
+    page.keyboard.press("Enter")
+    page.wait_for_timeout(5000)
+    html = page.inner_html("#projects")
+    soup = BeautifulSoup(html, "html.parser")
+
+    all_df = pd.DataFrame()
+    for url_end in [x.find("a")["href"] for x in soup.find_all(class_="open")][0:20]:
+        page.goto(f"https://mijn.freelance.nl{url_end}")
+        new_page_html = page.inner_html("#content")
+        new_page_soup = BeautifulSoup(new_page_html, "html.parser")
+        value_list = [x.text for x in new_page_soup.find_all("span", class_="value")]
+        name_list = [
+            x.text.replace("\n", "-").replace("  ", "")
+            for x in new_page_soup.find_all("span", class_="name")
+        ][0 : len(value_list)]
+        df = pd.DataFrame(
+            [new_page_soup.find("h1").text] + value_list,
+            index=["job_title"] + name_list,
+            columns=[0],
+        ).T.assign(
+            **{
+                "description": new_page_soup.find("div", class_="htmldescription")
+                .text.replace("\n", " ")
+                .replace("  ", "")
+            }
+        )
+        all_df = pd.concat([all_df, df])
+
+    all_df.to_csv(
+        f"freelance_nl_data/freelance_nl_{datetime.today().strftime('%Y-%m-%d')}.csv", index=False
+    )
