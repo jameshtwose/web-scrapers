6ddaa4247ae53856c05c786b8cde66eb55349d12
James <james@Jamess-MacBook-Air.local>
2023-05-24 16:44:59 +0200

    creating a dashboard for the job scraper

--- job_scrape/EDA.py
+++ job_scrape/EDA.py
@@ -1,6 +1,7 @@
 # %%
 import pandas as pd
 from glob import glob
+from datetime import datetime, timedelta
 import textwrap
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -17,9 +18,23 @@ column_list = [
     "Op locatie",
     "Looptijd",
     "Aantal uur",
+    "Reacties",
     "description",
 ]
-df = pd.concat([pd.read_csv(x) for x in glob("freelance_nl_data/*")]).loc[:, column_list]
+df = (
+    pd.concat([pd.read_csv(x) for x in glob("freelance_nl_data/*")])
+    .loc[:, column_list]
+    .assign(
+        **{
+            "Publicatiedatum": lambda x: pd.to_datetime(
+                x["Publicatiedatum"], format="%d-%m-%Y %H:%M"
+            ),
+            "Reacties": lambda x: x["Reacties"].str.split(" ", expand=True)[0].astype(int),
+        }
+    )
+    .sort_values("Publicatiedatum", ascending=False)
+    .reset_index(drop=True)
+)
 # %%
 df.head()
 # %%
@@ -31,10 +46,17 @@ _ = sns.countplot(y="Looptijd", data=df)
 # %%
 _ = sns.countplot(y="Geplaatst door", data=df)
 # %%
-plot_df = df.loc[lambda x: x["job_title"].str.contains("Data|data|Python"), :]
+plot_df = df.loc[lambda x: x["job_title"].str.contains("Data|data|Python|BI"), :]
 _ = sns.countplot(y="job_title", data=plot_df)
 # %%
-for i, x in enumerate(plot_df["job_title"].unique()):
+select_df = df.loc[
+    lambda x: x["Publicatiedatum"] >= datetime.now() - timedelta(days=1)
+].loc[lambda x: x["job_title"].str.contains("Data|data|Python|BI"), :]
+for i, x in enumerate(select_df["job_title"].unique()):
     print("\n", i, x)
     print("\n".join(textwrap.wrap(plot_df.iloc[i, :]["description"], width=50)))
+# %%
+select_df
+# %%
+select_df["Aantal uur"].value_counts()
 # %%
---
+++ job_scrape/dashboard.py
@@ -0,0 +1,193 @@
+# a streamlit dashboard to display the results of the job scrape
+# to run the dashboard locally, run the following command from the job_scrape directory:
+# streamlit run dashboard.py in the terminal
+
+import streamlit as st
+import pandas as pd
+from glob import glob
+import plotly.express as px
+
+# configure the page
+st.set_page_config(page_title="Freelance Jobs Dashboard", page_icon=":sparkles:", layout="wide")
+
+single_color_palette = ["#8f0fd4"]
+
+
+@st.cache_data(ttl=60 * 60 * 24)
+def load_data():
+    column_list = [
+        "job_title",
+        "Geplaatst door",
+        "Publicatiedatum",
+        "Startdatum",
+        "Op locatie",
+        "Looptijd",
+        "Aantal uur",
+        "Tarief",
+        "Contract",
+        "Reacties",
+        "description",
+    ]
+    return (
+        pd.concat([pd.read_csv(x) for x in glob("freelance_nl_data/*")])
+        .loc[:, column_list]
+        .assign(
+            **{
+                "Publicatiedatum": lambda x: pd.to_datetime(
+                    x["Publicatiedatum"], format="%d-%m-%Y %H:%M"
+                ),
+                "Reacties": lambda x: x["Reacties"].str.split(" ", expand=True)[0].astype(int),
+                "Geplaatst door": lambda x: x["Geplaatst door"].fillna("Onbekend"),
+            }
+        )
+        .sort_values("Publicatiedatum", ascending=False)
+        .reset_index(drop=True)
+    )
+
+
+# load the data
+df = load_data()
+
+# implement a sidebar
+st.sidebar.title("Job Scrape Dashboard")
+st.sidebar.markdown(
+    """
+    This dashboard displays the results of the job scrape.
+    """
+)
+chosen_jobs = st.sidebar.multiselect(
+    "Select a job title",
+    df["job_title"].unique(),
+    key="job_title",
+    default=df.loc[lambda x: x["job_title"].str.contains("Data|data"), "job_title"].unique(),
+    help="Select a job title to display the description.",
+)
+st.sidebar.markdown(
+    """
+    <div style="text-align: center; padding-right: 10px; padding-top: 20px;">
+        <img alt="logo" src="https://services.jms.rocks/img/logo.png" width="100">
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+st.sidebar.markdown(
+    """
+    <div style="text-align: center; color: #E8C003; margin-top: 40px; margin-bottom: 40px;">
+        <a href="https://services.jms.rocks" style="color: #E8C003;">Created by James Twose</a>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
+
+if chosen_jobs == []:
+    chosen_jobs = df["job_title"].unique()
+
+# main page
+st.title("Freelance Job Scrape Dashboard")
+
+# display all the data
+st.header("All Scraped Data")
+st.write(df)
+
+# plot the amount of jobs per Date
+st.header("Amount of Jobs per Date")
+st.write(
+    px.line(
+        df.assign(**{"date": lambda x: x["Publicatiedatum"].dt.date})
+        .groupby("date")
+        .agg({"job_title": "count"})
+        .sort_values("date", ascending=False)
+        .reset_index(),
+        x="date",
+        y="job_title",
+        title="Amount of Jobs per date",
+        color_discrete_sequence=single_color_palette,
+    )
+)
+
+# display the data for the selected job titles
+selected_df = df.loc[lambda x: x["job_title"].isin(chosen_jobs), :]
+st.header("Selected Job Title(s)")
+st.write(selected_df)
+
+# plot the geplaats door value counts for the selected job titles
+# st.write(
+#     px.bar(
+#         selected_df["Geplaatst door"].value_counts().reset_index(),
+#         x="count",
+#         y="Geplaatst door",
+#         title="Geplaatst door Value Counts for the Selected Job Titles",
+#     )
+# )
+st.markdown("#### People/ companies who posted the selected job titles:")
+st.markdown(
+    f"""<p style="color: {single_color_palette[0]}">{", "
+    .join(selected_df["Geplaatst door"].dropna().unique().tolist())}</p>""",
+    unsafe_allow_html=True,
+)
+
+# plot the looptijd value counts for the selected job titles
+st.write(
+    px.bar(
+        selected_df["Looptijd"].value_counts().reset_index(),
+        x="count",
+        y="Looptijd",
+        title="Looptijd Value Counts for the Selected Job Titles",
+        color_discrete_sequence=single_color_palette,
+    )
+)
+
+# plot the number of responses per job title
+st.write(
+    px.bar(
+        selected_df.sort_values("Reacties", ascending=False).reset_index(),
+        x="job_title",
+        y="Reacties",
+        color="Geplaatst door",
+        title="Number of Responses per Job Title of Selected Job Titles",
+    )
+)
+
+# plot the Aantal uur value counts for the selected job titles
+st.write(
+    px.bar(
+        selected_df["Aantal uur"].value_counts().reset_index(),
+        x="count",
+        y="Aantal uur",
+        title="Aantal uur Value Counts for the Selected Job Titles",
+        color_discrete_sequence=single_color_palette,
+    )
+)
+
+# plot the Tarief value counts for the selected job titles
+st.write(
+    px.bar(
+        selected_df["Tarief"].value_counts().reset_index(),
+        x="count",
+        y="Tarief",
+        title="Tarief Value Counts for the Selected Job Titles",
+        color_discrete_sequence=single_color_palette,
+    )
+)
+
+# plot the Contract value counts for the selected job titles
+st.write(
+    px.bar(
+        selected_df["Contract"].value_counts().reset_index(),
+        x="count",
+        y="Contract",
+        title="Contract Value Counts for the Selected Job Titles",
+        color_discrete_sequence=single_color_palette,
+    )
+)
+
+# hide the streamlit style
+hide_streamlit_style = """
+                        <style>
+                        #MainMenu {visibility: hidden;}
+                        footer {visibility: hidden;}
+                        header {visibility: hidden;}
+                        </style>
+                        """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
